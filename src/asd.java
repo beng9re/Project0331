90955ba91b024ceacb7ee96b9ee9e6e0fb9f3216
user\user1 <kbh0581@gmail.com>
2017-03-31 17:41:37 +0900

    최초버전

---
+++ src/asd.java
@@ -0,0 +1,10 @@
+
+package aa;
+import com.ss.driver.MiracleDriver;
+public class asd
+{
+	public static void main(String[] args){
+		MiracleDriver md= new MiracleDriver();
+		System.out.println(md.getName());
+	}
+}
---
+++ src/com/ss/driver/MiracleDriver.java
@@ -0,0 +1,14 @@
+package com.ss.driver;
+
+public class MiracleDriver{
+	private String name="나드라이버";
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+
+}
